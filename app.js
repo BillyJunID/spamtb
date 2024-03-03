20df90af9d64fe3f07fb20a87f0a553d99f7bcde
Billy Junaidi <billy.junaidi@gmail.com>
2024-03-04 01:16:10 +0800

    refractor

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const {default: axios} = require("axios")
-
-const url = "https://api.telegram.org/bot6680557007:AAGmHtzlEVGXJxKxxF62tUPFx9kmYeqk5QQ/sendMessage?parse_mode=markdown&chat_id=6465082908"
+const { default: axios } = require("axios");
 
 async function sendMessage() {
     try {
+        const url =
+            "https://api.telegram.org/bot6680557007:AAGmHtzlEVGXJxKxxF62tUPFx9kmYeqk5QQ/sendMessage?parse_mode=markdown&chat_id=6465082908";
+
         while (true) {
-            const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const chars =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             let email = "";
             for (let i = 0; i < 10; i++) {
                 email += chars.charAt(Math.floor(Math.random() * chars.length)).toUpperCase();
@@ -16,8 +18,10 @@ async function sendMessage() {
             for (let i = 0; i < 8; i++) {
                 password += chars.charAt(Math.floor(Math.random() * chars.length)) + Math.floor(Math.random() * 10);
             }
-            url += "&text=" + encodeURIComponent(`Email: ${email}\nPassword: ${password}`);
-            await axios.get(url)
+            const text = `Email: ${email}\nPassword: ${password}`;
+            const encodedText = encodeURIComponent(text);
+            const fullUrl = `${url}&text=${encodedText}`;
+            await axios.get(fullUrl)
                 .then((response) => {
                     if (response.status == 200) {
                         console.log(response)
