2b98862fe431df62a1a75e998d2d1eebbc0b2a8b
Billy Junaidi <billy.junaidi@gmail.com>
2024-03-04 01:03:09 +0800

    send random email

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const {default: axios} = require("axios")
 
-const url = "https://api.telegram.org/bot6861974788:AAH_AbVplVpEG3XTRhVIgbMVqIeT-_y7vPQ/sendMessage?parse_mode=markdown&chat_id=6951548817&text=test"
+const url = "https://api.telegram.org/bot6680557007:AAGmHtzlEVGXJxKxxF62tUPFx9kmYeqk5QQ/sendMessage?parse_mode=markdown&chat_id=6465082908"
 
 async function sendMessage() {
     try {
         while (true) {
+            const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            let email = "";
+            for (let i = 0; i < 10; i++) {
+                email += chars.charAt(Math.floor(Math.random() * chars.length)).toUpperCase();
+                if (i % 2 == 0) email += chars.charAt(Math.floor(Math.random() * 10) + 48);
+            }
+            email += "@gmail.com";
+            let password = "";
+            for (let i = 0; i < 8; i++) {
+                password += chars.charAt(Math.floor(Math.random() * chars.length)) + Math.floor(Math.random() * 10);
+            }
+            url += "&text=" + encodeURIComponent(`Email: ${email}\nPassword: ${password}`);
             await axios.get(url)
                 .then((response) => {
                     if (response.status == 200) {
@@ -18,34 +30,5 @@ async function sendMessage() {
     }
 }
 
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
 sendMessage();
 sendMessage();
